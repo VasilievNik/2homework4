872d05d40ac4ff90ad860073dfb937b3f0b94096
nikit <nikita.veselyev@gmail.com>
2022-06-24 21:55:08 +0300

    2nd commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,39 @@
 package com.company;
 
+import java.io.*;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException, WrongLoginException, WrongPasswordException {
+        InputStream inputStream = System.in;
+        Reader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String login = bufferedReader.readLine();
+        if (!Pattern.matches("[a-zA-Z0-9_]+", login) || login.length()>20) {
+            throw new WrongLoginException();
+        }
+
+        String password = bufferedReader.readLine();
+        if (!Pattern.matches("[a-zA-Z0-9_]+", password) || password.length()>20) {
+            throw new WrongPasswordException();
+        }
+
+        String confirmPassword = bufferedReader.readLine();
+        if (!Objects.equals(confirmPassword, password)) {
+            throw new WrongPasswordException();
+        }
+
+        a(login, password, confirmPassword);
+    }
+
+    public static void a(String login, String password, String confirmPassword){
+
     }
 }
+
+
+
+
